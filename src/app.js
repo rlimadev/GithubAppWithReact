6dc096d5202586459f0898bf24629fa18e58a18a
Rafael <rlimadev@gmail.com>
2018-08-21 19:29:09 -0300

    start react app

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
-const Title = React.createClass({
-  render() {
-    return (
-      React.createElement('h1', null, 'Meu primeiro programa')
-    );
-  },
-});
+const App = () => (
+  <div className="app">
+    <div className="user-info">
+      <img src="https://avatars1.githubusercontent.com/u/30198528?v=4" alt="Rafael Lima" />
+      <h2>
+        <a href="https://github.com/rlimadev">Rafael Lima</a>
+      </h2>
+    </div>
+  </div>
+);
 
-export default Title;
+export default App;
